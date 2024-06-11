b7df14649d68f9a00e6fe9f4642dba7f7dc011c2
alexa <alex@targoninc.com>
2024-06-11 21:46:58 +0200

    generation time

--- index.mjs
+++ index.mjs
@@ -1,12 +1,20 @@
 import {Generator} from "./Generator.mjs";
 import {Templates} from "./Templates.mjs";
+import {computedSignal, signal, store} from "https://fjs.targoninc.com/f.mjs";
 
 const canvas = document.getElementById("target");
+
+store().set("lastGenTime", signal(0));
+const buttonText = computedSignal(store().get("lastGenTime"), time => `Generate (${time.toFixed(2)}ms)`);
 
 const generator = new Generator(canvas);
 const controls = document.getElementById("controls");
 generator.getControlProperties().forEach(control => controls.appendChild(control));
-controls.appendChild(Templates.buttonWithIcon("refresh", "Generate", () => generator.generateImage()));
+controls.appendChild(Templates.buttonWithIcon("refresh", buttonText, () => {
+    const start = performance.now();
+    generator.generateImage();
+    store().setSignalValue("lastGenTime", performance.now() - start);
+}));
 controls.appendChild(
     Templates.buttonWithIcon("file_download", "Download", () => {
         const a = document.createElement("a");
